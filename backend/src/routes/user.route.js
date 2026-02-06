cc4119c911b52a1e72bfb78835b9a31a4a5d539e
leechunsiang <leechunsiang0724@gmail.com>
2026-02-07 04:06:27 +0800

    user routes added

---
+++ backend/src/routes/user.route.js
@@ -0,0 +1,30 @@
+import { Router } from "express"
+import { protectRoute } from "../middleware/auth.middleware"
+import {
+  addAddress,
+  deleteAddress,
+  getAddresses,
+  updateAddress,
+  addToWishlist,
+  removeFromWishlist,
+  getWishlist,
+} from "../controllers/user.controller.js"
+import { get } from "mongoose"
+
+const router = Router()
+
+router.use(protectRoute)
+
+//adress routes
+router.post("/addresses", addAddress)
+router.get("/addresses", getAddresses)
+router.put("/addresses/:addressId", updateAddress)
+router.delete("/addresses/:addressId", deleteAddress)
+
+//wishlist routes
+
+router.post("/wishlist", addToWishlist)
+router.delete("/wishlist/:productId", removeFromWishlist)
+router.get("/wishlist", getWishlist)
+
+export default router
